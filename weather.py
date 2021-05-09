993cac8202a11be9cccb9e3bdf37df8864c63071
santosrodriguez <37907123+santosrodriguez@users.noreply.github.com>
2021-05-09 08:45:04 -0400

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,13 @@
+# Notice that there are 3 print statements. The last statement is separate from the function because its not indented. The function runs last even though its printed second.
+# 
+
+
+print("Checking the weather for you!")
+
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+
+weather_check()
